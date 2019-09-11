1a2a1e521595bc471ccc2baf016f5ff40933dca0
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-11 18:10:23 -0300

    parts: Follow Android 10 changes on switchbar
    Change-Id: Ifad28dc874233eb41d89580895b769ecbc3ea670

--- device.mk
+++ device.mk
@@ -14,10 +14,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-bootleg
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-bootleg
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
--- extract-files.sh
+++ extract-files.sh
@@ -16,7 +16,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 AOSP_ROOT="${MY_DIR}"/../../..
 
-HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${MK_ROOT}/vendor/bootleggers/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -53,11 +53,11 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${MK_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
-BLOB_ROOT="${AOSP_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
+BLOB_ROOT="${MK_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
 
 "${MY_DIR}/setup-makefiles.sh"
